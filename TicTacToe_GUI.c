6ad04ccdcac7a9b8ff1b65bb883d658a03216259
JRRZZ <115517434+JRRZZ@users.noreply.github.com>
2022-10-29 01:39:47 +0800

    Update TicTacToe_GUI.c

--- TicTacToe_GUI.c
+++ TicTacToe_GUI.c
@@ -317,3 +317,29 @@ int main(int argc, char**argv){
 
   return status;
 }
+
+
+//minimax algorithm//
+/*int minimax(int board[9], int player) { //P vs Minimax
+    //How is the position like for player (their turn) on board?
+    int winner = checkWin(board);        // getting the output from checkwinner func, '0' for no winner 
+    if(winner != 0)                 // checking if theres winner 
+    return winner*player;                 // return if theres winner 
+    
+    int move = -1;
+    int score = -2;     //Losing moves are preferred to no move
+    int i;
+    for(i = 0; i < 9; ++i) {            //For all moves,
+        if(board[i] == 0) {         //If unoccupied
+            board[i] = player;          //Try the move
+            int thisScore = -minimax(board, player*-1);   // calculate the score for the move
+            if(thisScore > score) {       // check if score is lesser than prev
+                score = thisScore;      // minimising the score 
+                move = i;            // replace old i with new updated i if true
+            }               
+            board[i] = 0;           //Reset move after trying
+        }
+    }
+    if(move == -1) return 0;
+    return score;
+}
